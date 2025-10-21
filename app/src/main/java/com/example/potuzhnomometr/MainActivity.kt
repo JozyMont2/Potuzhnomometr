8906ad0c79035454e5ad27a3e4ed3dfe61bbfa2e
JozyMont <korolv900@gmail.com>
2025-10-21 16:08:50 +0300

    Added transition to the zradomometr

--- app/src/main/java/com/example/potuzhnomometr/MainActivity.kt
+++ app/src/main/java/com/example/potuzhnomometr/MainActivity.kt
@@ -34,6 +34,12 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
+        zrd.setOnClickListener {
+            val intent = Intent(this, Zradomometr::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
