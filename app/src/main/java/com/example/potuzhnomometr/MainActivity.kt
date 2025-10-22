b59163ca907097000639a2c1e5181255ab326e2d
JozyMont <korolv900@gmail.com>
2025-10-22 09:48:44 +0300

    Updating UI for main screen

--- app/src/main/java/com/example/potuzhnomometr/MainActivity.kt
+++ app/src/main/java/com/example/potuzhnomometr/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.potuzhnomometr
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val ptzhn: Button = findViewById(R.id.button_ptzh)
+        val zrd: Button = findViewById(R.id.button_zrd)
+        val zrd_ptzhn: Button = findViewById(R.id.button_zrd_ptzhn)
+        val test: Button = findViewById(R.id.button_test)
+
+        ptzhn.setOnClickListener {
+            val intent1 = Intent(this, Potuzhnomometr)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
