3f627367f3429bb5acf1e71942c08e9fbfe9db46
JozyMont <korolv900@gmail.com>
2025-10-26 13:34:02 +0200

    Fixed bags of main screen

--- app/src/main/java/com/example/potuzhnomometr/MainActivity.kt
+++ app/src/main/java/com/example/potuzhnomometr/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.potuzhnomometr
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +19,26 @@ class MainActivity : AppCompatActivity() {
         val zrd: Button = findViewById(R.id.button_zrd)
         val zrd_ptzhn: Button = findViewById(R.id.button_zrd_ptzhn)
         val test: Button = findViewById(R.id.button_test)
+
+        val chilomometr: ImageView = findViewById(R.id.imageView_chilomometr)
+        val krinzhomometr: ImageView = findViewById(R.id.imageView_krinzhomometr)
+        val radmomaizer_of_mood: ImageView = findViewById(R.id.imageView_radmomaizer_of_mood)
+
+        val more_details: Button = findViewById(R.id.button_more_details)
+        val about_app: Button = findViewById(R.id.button_about_app)
+        val about_developer: Button = findViewById(R.id.button_about_developer)
+
+        ptzhn.setOnClickListener {
+            val intentOne = Intent(this, Potuzhnomometr::class.java)
+            startActivity(intentOne)
+            finish()
+        }
+
+        zrd.setOnClickListener {
+            val intentTwo = Intent(this, Zradomometr::class.java)
+            startActivity(intentTwo)
+            finish()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
